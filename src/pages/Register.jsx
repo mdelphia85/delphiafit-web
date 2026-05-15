103c95313a05e4229e1d09081b5f247db846b59d
Matthew Delphia <delphia.matthew6@gmail.com>
2026-05-15 18:17:45 -0400

    Fix register payload to include name

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -54,7 +54,7 @@ export default function Register() {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password }),
+          body: JSON.stringify({ name, email, password }),
         }
       );
 
